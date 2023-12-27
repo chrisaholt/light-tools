7a2f2f0b448fdfe51b62af2fbae11898ce37f35b
Chris Aholt <35589544+chrisaholt@users.noreply.github.com>
2023-12-27 14:45:04 -0600

    Moves base Surface class to own file

--- examples/wave_propagation_2d.py
+++ examples/wave_propagation_2d.py
@@ -7,6 +7,10 @@ import os
 import plotly.express as px
 import plotly.graph_objects as go
 
+import sys
+sys.path.append("..")
+
+from geometry.surface import Surface
 
 def expand_shape(array: np.array):
     """Expand array's shape to be one more dimension."""
@@ -26,48 +30,6 @@ def scale_func(
     optical_distance_to_point: np.array,
 ):
     return 1 / (1 + optical_distance_to_point)
-
-class Surface:
-    """
-    Describes a surface in space.
-    """
-    def __init__(self):
-        pass
-
-    def intersect(self,
-        ray_start: np.array,
-        ray_dir: np.array,
-    ):
-        """
-        Computes the intersection of a ray with this surface.
-
-        Args:
-            ray_start (np.array (num_points, point_dimension)): array of ray origins
-            ray_dir (np.array (num_points, point_dimension)): array of ray directions
-
-        Returns:
-            intersection (np.array (num_points, point_dimension))
-            is_valid (np.array[bool] (num_points,)): True iff the rays intersect along the positive ray direction.
-        """
-        raise NotImplementedError()
-
-    def normal_at(self,
-        point: np.array,
-    ):
-        """
-        Computes the unit vector normal to the surface at the given point.
-        If the point is not on the surface, then the returned value will be
-        the normal at the closest point on the surface.
-        """
-        raise NotImplementedError()
-
-    def closest_point(self,
-        point: np.array,
-    ):
-        """
-        Computes the closest point on the surface to the given point.
-        """
-        raise NotImplementedError()
 
 class VerticalPlane(Surface):
     """
---
+++ geometry/surface.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Surface:
+    """
+    Describes a surface in space.
+    """
+    def __init__(self):
+        pass
+
+    def intersect(self,
+        ray_start: np.array,
+        ray_dir: np.array,
+    ):
+        """
+        Computes the intersection of a ray with this surface.
+
+        Args:
+            ray_start (np.array (num_points, point_dimension)): array of ray origins
+            ray_dir (np.array (num_points, point_dimension)): array of ray directions
+
+        Returns:
+            intersection (np.array (num_points, point_dimension))
+            is_valid (np.array[bool] (num_points,)): True iff the rays intersect along the positive ray direction.
+        """
+        raise NotImplementedError()
+
+    def normal_at(self,
+        point: np.array,
+    ):
+        """
+        Computes the unit vector normal to the surface at the given point.
+        If the point is not on the surface, then the returned value will be
+        the normal at the closest point on the surface.
+        """
+        raise NotImplementedError()
+
+    def closest_point(self,
+        point: np.array,
+    ):
+        """
+        Computes the closest point on the surface to the given point.
+        """
+        raise NotImplementedError()
